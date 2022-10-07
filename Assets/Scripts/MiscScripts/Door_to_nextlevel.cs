5eb9c61aac6797d92863b0d646b7e5ffd11994f1
Justin Li <107675909+Justinyutingli@users.noreply.github.com>
2022-10-06 21:48:09 -0400

    Door Code to next level
    Update the door code to the next level

---
+++ Assets/Scripts/MiscScripts/Door_to_nextlevel.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door_to_nextlevel : MonoBehaviour
+{
+    public string levelToLoad;
+    public bool locked = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (!locked)
+            {
+                SceneManager.LoadScene(levelToLoad);
+            } else {
+                if (PublicVars.Keys > 0){
+                    PublicVars.Keys--;
+                    SceneManager.LoadScene(levelToLoad);
+                }
+            }
+        }
+    }
+}
