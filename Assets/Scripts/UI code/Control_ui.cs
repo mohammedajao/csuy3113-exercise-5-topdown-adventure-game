a538f4169a05a1e006c392cc168ab589e7780eed
Justin Li <107675909+Justinyutingli@users.noreply.github.com>
2022-10-06 23:55:59 -0400

    the end-game function added
    the end-game funcionality now will work accordingly.

---
+++ Assets/Scripts/UI code/Control_ui.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Control_ui : MonoBehaviour
+{
+    public void ResetTheGame()
+    {
+        SceneManager.LoadScene("BaseScene"); 
+        print("The button is working.");
+    }
+
+    
+    public void GoMainMenu()
+    {
+        SceneManager.LoadScene("BaseScene");
+    }
+}
